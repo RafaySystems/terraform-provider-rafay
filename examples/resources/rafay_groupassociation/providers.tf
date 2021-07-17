6e0a318d16b6dc848dddfb4f72452f163f6e3846
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2021-07-16 18:11:02 -0700

    Added docs

--- examples/resources/rafay_groupassociation/providers.tf
+++ examples/resources/rafay_groupassociation/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 variable "rafay_config_file" {
   description = "rafay provider config file for authentication"
-  default = "/Users/sougat/Downloads/rafay_qa-sougat@rafay.co.json"
+  default     = "<rafay-config-json-file>"
   sensitive   = true
 }
 

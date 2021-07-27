a8e401d1109808f4092f669122c7fd8118e3c7a4
sougat-rafay <sougat@rafay.co>
2021-07-27 10:53:26 -0700

    applied bootstrap yaml to cluster

--- examples/resources/rafay_import_cluster/resource.tf
+++ examples/resources/rafay_import_cluster/resource.tf
@@ -3,6 +3,6 @@ resource "rafay_import_cluster" "import_cluster" {
   projectname      = "dev1-proj"
   blueprint        = "default"
   location         = "losangeles-us"
-  kube_config_path = ""
+  kube_config_path = "/Users/sougat/Downloads/import_cluster_kubeconfig.yaml"
   description      = ""
 }

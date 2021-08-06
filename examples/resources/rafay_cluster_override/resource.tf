167536f322df8255cb0223c5572ebfdd4c76cf16
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2021-08-05 21:40:35 -0700

    Modified example to remove local file path

--- examples/resources/rafay_cluster_override/resource.tf
+++ examples/resources/rafay_cluster_override/resource.tf
@@ -1,5 +1,5 @@
 resource "rafay_cluster_override" "cluster_override" {
   projectname               = "dev1-proj"
-  cluster_override_filepath = "/Users/sougat/Downloads/rafay_workload_cluster_overrides/cli/nginx_cluster_override_1.yaml"
+  cluster_override_filepath = "<file-path/cluster_override.yaml>"
 }
 #cluster_override_filepath is the local filepath to the override.yaml file we want to add 

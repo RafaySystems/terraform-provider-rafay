a1f37007de3b861f8635734dff15cb0bfb0daa65
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2021-08-13 16:35:40 -0700

    Modified readmes

--- examples/resources/rafay_eks_cluster/resource.tf
+++ examples/resources/rafay_eks_cluster/resource.tf
@@ -1,5 +1,5 @@
 resource "rafay_eks_cluster" "cluster" {
-  name             = "demo-terraform"
-  projectname      = "dev"
-  yamlfilepath     = "/Users/krishna/code/src/github.com/RafaySystems/terraform-provider-rafay/examples/resources/rafay_eks_cluster/eks-cluster.yaml"
+  name         = "demo-terraform"
+  projectname  = "dev"
+  yamlfilepath = "<filepath/eks-cluster.yaml>"
 }

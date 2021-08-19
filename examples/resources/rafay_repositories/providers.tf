fdfc2b7ea552be10493752d1b913c10dfedc42fc
sougat-rafay <sougat@rafay.co>
2021-08-18 22:05:16 -0700

    Finished testing create, update, and delete

--- examples/resources/rafay_repositories/providers.tf
+++ examples/resources/rafay_repositories/providers.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     rafay = {
       version = ">= 0.1"
-      source  = "registry.terraform.io/Rafay/rafay"
+      source  = "registry.terraform.io/RafaySystems/rafay"
     }
   }
 }
 
 variable "rafay_config_file" {
   description = "rafay provider config file for authentication"
-  default     = "/Users/sougat/Downloads/rafay_qa-sougat@rafay.co.json"
+  default     = "<rafay-config-json-file>"
   sensitive   = true
 }
 

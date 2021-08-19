1214ba12122545c2b48c5e245cd8c4625c361d76
sougat-rafay <sougat@rafay.co>
2021-08-18 22:00:21 -0700

    finish create and delete

--- examples/resources/rafay_repositories/resource.tf
+++ examples/resources/rafay_repositories/resource.tf
@@ -1,5 +1,5 @@
 resource "rafay_repositories" "repositories" {
-  projectname         = "dev1-proj"
-  repository_filepath = "/Users/sougat/Downloads/repository.yaml>"
+  projectname               = "dev1-proj"
+  repository_filepath = "/Users/sougat/Downloads/repository.yaml"
 }
 #repository_filepath is the local filepath to the repository.yaml file we want to add 

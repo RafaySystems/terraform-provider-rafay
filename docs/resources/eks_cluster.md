12147f6dd49789700bf7f4b13203ad801efc6b8a
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2021-08-23 07:53:31 -0700

    Reverted unwanted chnages

--- docs/resources/eks_cluster.md
+++ docs/resources/eks_cluster.md
@@ -16,7 +16,7 @@ description: |-
 resource "rafay_eks_cluster" "cluster" {
   name         = "demo-terraform"
   projectname  = "dev"
-  yamlfilepath = "<filepath/eks-cluster.yaml>"
+  yamlfilepath = "<file-path/eks-cluster.yaml>"
   waitflag     = "1"
 }
 ```
--- examples/resources/rafay_agent/resource.tf
+++ examples/resources/rafay_agent/resource.tf
@@ -1,5 +1,5 @@
 resource "rafay_agent" "agent" {
   projectname    = "defaultproject"
-  agent_filepath = "/Users/sougat/Downloads/agent.yaml"
+  agent_filepath = "<file=path>/agent.yaml"
 }
 #agent_filepath is the local filepath to the agent.yaml file we want to add

db97d1379b3e1efef758fe1e66054a366b2e7fac
sougat-rafay <sougat@rafay.co>
2021-09-21 15:26:18 -0700

    Finished testing infra-provisioner

---
+++ examples/resources/rafay_infrastructure_provisioner/resource.tf
@@ -0,0 +1,5 @@
+resource "rafay_infrastructure_provisioner" "infrastructure_provisioner" {
+  projectname                        = "dev1-proj"
+  infrastucture_provisioner_filepath = "/Users/sougat/Downloads/infraprovisioner.yaml"
+}
+#infrastucture_provisioner_filepath is the local filepath to the infrastucture_provisioner.yaml file we want to add 

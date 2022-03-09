b53ecac613054fc84573801bcc6740c81e4e7be0
Hardik Italia <hardik@rafay.co>
2022-03-08 18:19:10 -0800

    Updated examples for project & namespace

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     rafay = {
       version = ">= 0.1"
-      source  = "RafaySystems/rafay"
+      source  = "registry.terraform.io/RafaySystems/rafay"
     }
   }
 }
 
 provider "rafay" {
-  provider_config_file = "/Users/cangadala/Downloads/rafay_qa-cangadala@rafay.co.json"
+  provider_config_file = "/Users/user1/oraganization-user@rafay.co.json"
 }

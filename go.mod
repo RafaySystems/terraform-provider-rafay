3cacafe45da475162eddf0bf77d2bd80b74256f4
Chaitanya Angadala <cangadala@rafay.co>
2022-05-03 16:16:44 -0700

    opa changes

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/RafaySystems/terraform-provider-rafay
 go 1.17
 
 require (
-	github.com/RafaySystems/rafay-common v1.11.0-terraform-2.0.20220301095714-908965fe245a
+	github.com/RafaySystems/rafay-common v1.13.0-beta5
 	//github.com/RafaySystems/rafay-common v1.10.0-tf-schema-beta1
 	github.com/RafaySystems/rctl v1.12.0-beta2
 	github.com/davecgh/go-spew v1.1.1
@@ -138,7 +138,7 @@ require (
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
@@ -151,15 +151,15 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/api v1.16.4 // indirect
-	k8s.io/apiextensions-apiserver v0.23.1 // indirect
+	k8s.io/apiextensions-apiserver v0.23.4 // indirect
 	k8s.io/client-go v12.0.0+incompatible // indirect
-	k8s.io/component-base v0.23.1 // indirect
-	k8s.io/klog/v2 v2.30.0 // indirect
+	k8s.io/component-base v0.23.3 // indirect
+	k8s.io/klog/v2 v2.40.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20211115234752-e816edb12b65 // indirect
-	k8s.io/utils v0.0.0-20210930125809-cb0fa318a74b // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/controller-runtime v0.11.0 // indirect
 	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
-	sigs.k8s.io/structured-merge-diff/v4 v4.2.0 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
---
+++ rafay/resource_opa_policy.go
@@ -0,0 +1,605 @@
+package rafay
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/RafaySystems/rafay-common/pkg/hub/client/options"
+	typed "github.com/RafaySystems/rafay-common/pkg/hub/client/typed"
+	"github.com/RafaySystems/rafay-common/pkg/hub/terraform/resource"
+	"github.com/RafaySystems/rafay-common/proto/types/hub/commonpb"
+	"github.com/RafaySystems/rafay-common/proto/types/hub/opapb"
+	"github.com/RafaySystems/rctl/pkg/config"
+	"github.com/RafaySystems/rctl/pkg/versioninfo"
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceOPAPolicy() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceOPAPolicyCreate,
+		ReadContext:   resourceOPAPolicyRead,
+		UpdateContext: resourceOPAPolicyUpdate,
+		DeleteContext: resourceOPAPolicyDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		SchemaVersion: 1,
+		Schema:        resource.OPAPolicySchema.Schema,
+	}
+}
+
+func resourceOPAPolicyCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("resourceOPAConstraintCreate create starts")
+	diags := resourceOPAPolicyUpsert(ctx, d, m)
+	if diags.HasError() {
+		tflog := os.Getenv("TF_LOG")
+		if tflog == "TRACE" || tflog == "DEBUG" {
+			ctx = context.WithValue(ctx, "debug", "true")
+		}
+		log.Printf("Opa policy create got error, perform cleanup")
+		ss, err := expandOPAPolicy(d)
+		if err != nil {
+			log.Printf("Opa policy expandOPAPolicy error")
+			return diags
+		}
+		auth := config.GetConfig().GetAppAuthProfile()
+		client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+		if err != nil {
+			return diags
+		}
+
+		err = client.OpaV3().OPAPolicy().Delete(ctx, options.DeleteOptions{
+			Name:    ss.Metadata.Name,
+			Project: ss.Metadata.Project,
+		})
+		if err != nil {
+			return diags
+		}
+	}
+	return diags
+}
+
+func resourceOPAPolicyUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("Opa policy update starts")
+	return resourceOPAPolicyUpsert(ctx, d, m)
+}
+
+func resourceOPAPolicyUpsert(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	log.Printf("Opa policy upsert starts")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	opaPolicy, err := expandOPAPolicy(d)
+	if err != nil {
+		log.Printf("Opa policy expandOPAPolicy error")
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.OpaV3().OPAPolicy().Apply(ctx, opaPolicy, options.ApplyOptions{})
+	if err != nil {
+		// XXX Debug
+		n1 := spew.Sprintf("%+v", opaPolicy)
+		log.Println("Opa policy apply Opa policy:", n1)
+		log.Printf("Opa policy apply error")
+		return diag.FromErr(err)
+	}
+
+	d.SetId(opaPolicy.Metadata.Name)
+	return diags
+
+}
+
+func resourceOPAPolicyRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	log.Println("resourceOPAPolicyRead ")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+	tfOPAPolicyState, err := expandOPAPolicy(d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	ag, err := client.OpaV3().OPAPolicy().Get(ctx, options.GetOptions{
+		Name:    tfOPAPolicyState.Metadata.Name,
+		Project: tfOPAPolicyState.Metadata.Project,
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = flattenOPAPolicy(d, ag)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return diags
+
+}
+
+func resourceOPAPolicyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	ag, err := expandOPAPolicy(d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.OpaV3().OPAPolicy().Delete(ctx, options.DeleteOptions{
+		Name:    ag.Metadata.Name,
+		Project: ag.Metadata.Project,
+	})
+
+	return diags
+}
+
+func expandOPAPolicy(in *schema.ResourceData) (*opapb.OPAPolicy, error) {
+	if in == nil {
+		return nil, fmt.Errorf("%s", "expand Opa policy empty input")
+	}
+	obj := &opapb.OPAPolicy{}
+
+	if v, ok := in.Get("metadata").([]interface{}); ok && len(v) > 0 {
+		obj.Metadata = expandMetaData(v)
+	}
+
+	if v, ok := in.Get("spec").([]interface{}); ok && len(v) > 0 {
+		objSpec, err := expandOPAPolicySpec(v)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("expandOPAPolicySpec got spec")
+		obj.Spec = objSpec
+	}
+
+	obj.ApiVersion = "infra.k8smgmt.io/v3"
+	obj.Kind = "OPAPolicy"
+	return obj, nil
+}
+
+func expandOPAPolicySpec(p []interface{}) (*opapb.OPAPolicySpec, error) {
+	obj := &opapb.OPAPolicySpec{}
+	if len(p) == 0 || p[0] == nil {
+		return obj, fmt.Errorf("%s", "expandOPAPolicySpec empty input")
+	}
+
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["sharing"].([]interface{}); ok && len(v) > 0 {
+		obj.Sharing = expandSharingSpec(v)
+	}
+
+	if v, ok := in["version"].(string); ok && len(v) > 0 {
+		obj.Version = v
+	}
+
+	if v, ok := in["installation_params"].([]interface{}); ok && len(v) > 0 {
+		obj.InstallationParams = expandOpaPolicyInstallationParams(v)
+	}
+
+	if v, ok := in["sync_objects"].([]interface{}); ok && len(v) > 0 {
+		obj.SyncObjects = expandOpaPolicySyncObjects(v)
+	}
+
+	if v, ok := in["excluded_namespaces"].([]interface{}); ok && len(v) > 0 {
+		obj.ExcludedNamespaces = expandOpaPolicyExcludedNamespaces(v)
+	}
+
+	if v, ok := in["constraint_list"].([]interface{}); ok && len(v) > 0 {
+		obj.ConstraintList = expandOpaPolicyConstraintList(v)
+	}
+
+	return obj, nil
+}
+
+func expandOpaPolicySyncObjects(p []interface{}) []*opapb.SyncObject {
+	if len(p) == 0 || p[0] == nil {
+		return []*opapb.SyncObject{}
+	}
+
+	out := make([]*opapb.SyncObject, len(p))
+
+	for i := range p {
+		obj := opapb.SyncObject{}
+		in := p[i].(map[string]interface{})
+
+		if v, ok := in["version"].(string); ok && len(v) > 0 {
+			obj.Version = v
+		}
+
+		if v, ok := in["group"].(string); ok && len(v) > 0 {
+			obj.Group = v
+		}
+
+		if v, ok := in["kind"].(string); ok && len(v) > 0 {
+			obj.Kind = v
+		}
+
+		out[i] = &obj
+
+	}
+
+	return out
+}
+
+func expandOpaPolicyInstallationParams(p []interface{}) *opapb.InstallationParams {
+	obj := &opapb.InstallationParams{}
+	if len(p) == 0 || p[0] == nil {
+		return obj
+	}
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["audit_interval"].(int64); ok {
+		obj.AuditInterval = v
+	}
+
+	if v, ok := in["constraint_violations_limit"].(int64); ok {
+		obj.ConstraintViolationsLimit = v
+	}
+
+	if v, ok := in["audit_interval"].(bool); ok {
+		obj.AuditFromCache = v
+	}
+
+	if v, ok := in["audit_match_kind_only"].(bool); ok {
+		obj.AuditMatchKindOnly = v
+	}
+
+	if v, ok := in["enable_delete_operations"].(bool); ok {
+		obj.EnableDeleteOperations = v
+	}
+
+	if v, ok := in["audit_chunk_size"].(int64); ok {
+		obj.AuditChunkSize = v
+	}
+
+	if v, ok := in["experimental_enable_mutation"].(bool); ok {
+		obj.ExperimentalEnableMutation = v
+	}
+
+	if v, ok := in["log_denies"].(bool); ok {
+		obj.LogDenies = v
+	}
+
+	if v, ok := in["emit_admission_events"].(bool); ok {
+		obj.EmitAdmissionEvents = v
+	}
+
+	if v, ok := in["emit_audit_events"].(bool); ok {
+		obj.EmitAuditEvents = v
+	}
+
+	return obj
+}
+
+func expandOpaPolicyExcludedNamespaces(p []interface{}) []*opapb.ExcludedNamespaces {
+	if len(p) == 0 || p[0] == nil {
+		return []*opapb.ExcludedNamespaces{}
+	}
+
+	out := make([]*opapb.ExcludedNamespaces, len(p))
+
+	for i := range p {
+		obj := opapb.ExcludedNamespaces{}
+		in := p[i].(map[string]interface{})
+
+		if v, ok := in["processes"].([]interface{}); ok && len(v) > 0 {
+			obj.Processes = toArrayString(v)
+		}
+
+		if v, ok := in["namespaces"].([]interface{}); ok && len(v) > 0 {
+			obj.Namespaces = expandOpaPolicyExcludedNamespacesList(v)
+		}
+
+		out[i] = &obj
+
+	}
+
+	return out
+}
+
+func expandOpaPolicyExcludedNamespacesList(p []interface{}) []*commonpb.ResourceRef {
+	if len(p) == 0 || p[0] == nil {
+		return []*commonpb.ResourceRef{}
+	}
+
+	out := make([]*commonpb.ResourceRef, len(p))
+
+	for i := range p {
+		obj := commonpb.ResourceRef{}
+		in := p[i].(map[string]interface{})
+
+		if v, ok := in["name"].(string); ok && len(v) > 0 {
+			obj.Name = v
+		}
+
+		out[i] = &obj
+	}
+	return out
+}
+
+func expandOpaPolicyConstraintList(p []interface{}) []*commonpb.ResourceRef {
+	if len(p) == 0 || p[0] == nil {
+		return []*commonpb.ResourceRef{}
+	}
+
+	out := make([]*commonpb.ResourceRef, len(p))
+
+	for i := range p {
+		obj := commonpb.ResourceRef{}
+		in := p[i].(map[string]interface{})
+
+		if v, ok := in["name"].(string); ok && len(v) > 0 {
+			obj.Name = v
+		}
+
+		out[i] = &obj
+	}
+	return out
+}
+
+// Flatten
+
+func flattenOPAPolicy(d *schema.ResourceData, in *opapb.OPAPolicy) error {
+	if in == nil {
+		return nil
+	}
+
+	err := d.Set("metadata", flattenMetaData(in.Metadata))
+	if err != nil {
+		return err
+	}
+
+	v, ok := d.Get("spec").([]interface{})
+	if !ok {
+		v = []interface{}{}
+	}
+
+	var ret []interface{}
+	ret, err = flattenOPAPolicySpec(in.Spec, v)
+	if err != nil {
+		return err
+	}
+
+	err = d.Set("spec", ret)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func flattenOPAPolicySpec(in *opapb.OPAPolicySpec, p []interface{}) ([]interface{}, error) {
+	if in == nil {
+		return nil, fmt.Errorf("%s", "flattenOPAPolicy empty input")
+	}
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if len(in.Version) > 0 {
+		obj["version"] = in.Version
+	}
+
+	if in.InstallationParams != nil {
+		v, ok := obj["default_addons"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["default_addons"] = flattenInstallationParams(in.InstallationParams, v)
+	}
+
+	if in.Sharing != nil {
+		obj["sharing"] = flattenSharingSpec(in.Sharing)
+	}
+
+	if in.SyncObjects != nil && len(in.SyncObjects) > 0 {
+		v, ok := obj["sync_objects"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["sync_objects"] = flattenSyncObjects(in.SyncObjects, v)
+	}
+
+	if in.ExcludedNamespaces != nil && len(in.ExcludedNamespaces) > 0 {
+		v, ok := obj["excluded_namespaces"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["excluded_namespaces"] = flattenExcludedNamespaces(in.ExcludedNamespaces, v)
+	}
+
+	if in.ConstraintList != nil && len(in.ConstraintList) > 0 {
+		v, ok := obj["constraint_list"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["constraint_list"] = flattenConstraintList(in.ConstraintList, v)
+	}
+
+	return []interface{}{obj}, nil
+}
+
+func flattenInstallationParams(in *opapb.InstallationParams, p []interface{}) []interface{} {
+	if in == nil {
+		return nil
+	}
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if in.AuditFromCache {
+		obj["audit_from_cache"] = in.AuditFromCache
+	}
+
+	if in.AuditMatchKindOnly {
+		obj["audit_match_kind_only"] = in.AuditMatchKindOnly
+	}
+
+	if in.EnableDeleteOperations {
+		obj["enable_delete_operations"] = in.EnableDeleteOperations
+	}
+
+	if in.ExperimentalEnableMutation {
+		obj["experimental_enable_mutation"] = in.ExperimentalEnableMutation
+	}
+
+	if in.AuditFromCache {
+		obj["audit_from_cache"] = in.AuditFromCache
+	}
+
+	obj["audit_chunk_size"] = in.AuditChunkSize
+
+	obj["audit_interval"] = in.AuditInterval
+
+	obj["constraint_violations_limit"] = in.ConstraintViolationsLimit
+
+	return []interface{}{obj}
+}
+
+func flattenExcludedNamespaces(input []*opapb.ExcludedNamespaces, p []interface{}) []interface{} {
+	log.Println("flattenExcludedNamespaces")
+	if input == nil {
+		return nil
+	}
+	out := make([]interface{}, len(input))
+	for i, in := range input {
+
+		obj := map[string]interface{}{}
+		if i < len(p) && p[i] != nil {
+			obj = p[i].(map[string]interface{})
+		}
+
+		if in.Namespaces != nil && len(in.Namespaces) > 0 {
+			v, ok := obj["namespaces"].([]interface{})
+			if !ok {
+				v = []interface{}{}
+			}
+			obj["namespaces"] = flattenNamespacesRef(in.Namespaces, v)
+		}
+
+		if len(in.Processes) > 0 {
+			obj["processes"] = toArrayInterface(in.Processes)
+		}
+
+		out[i] = &obj
+	}
+
+	return out
+}
+
+func flattenNamespacesRef(input []*commonpb.ResourceRef, p []interface{}) []interface{} {
+	log.Println("flattenNamespacesRef")
+	if input == nil {
+		return nil
+	}
+	out := make([]interface{}, len(input))
+	for i, in := range input {
+
+		obj := map[string]interface{}{}
+		if i < len(p) && p[i] != nil {
+			obj = p[i].(map[string]interface{})
+		}
+
+		if len(in.Name) > 0 {
+			obj["name"] = in.Name
+		}
+		out[i] = &obj
+	}
+
+	return out
+}
+
+func flattenConstraintList(input []*commonpb.ResourceRef, p []interface{}) []interface{} {
+	log.Println("flattenConstraintList")
+	if input == nil {
+		return nil
+	}
+	out := make([]interface{}, len(input))
+	for i, in := range input {
+
+		obj := map[string]interface{}{}
+		if i < len(p) && p[i] != nil {
+			obj = p[i].(map[string]interface{})
+		}
+
+		if len(in.Name) > 0 {
+			obj["name"] = in.Name
+		}
+
+		out[i] = &obj
+	}
+
+	return out
+}
+
+func flattenSyncObjects(input []*opapb.SyncObject, p []interface{}) []interface{} {
+	log.Println("flattenSyncObjects")
+	if input == nil {
+		return nil
+	}
+	out := make([]interface{}, len(input))
+	for i, in := range input {
+
+		obj := map[string]interface{}{}
+		if i < len(p) && p[i] != nil {
+			obj = p[i].(map[string]interface{})
+		}
+
+		if len(in.Group) > 0 {
+			obj["group"] = in.Group
+		}
+
+		if len(in.Version) > 0 {
+			obj["version"] = in.Version
+		}
+
+		if len(in.Kind) > 0 {
+			obj["kind"] = in.Kind
+		}
+
+		out[i] = &obj
+	}
+
+	return out
+}

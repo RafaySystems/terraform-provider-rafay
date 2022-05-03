97ae451f46d077814bd76b908d17eed32763761b
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-05-03 14:55:12 -0700

    fixed obj.EnableDetailedMonitoring

--- rafay/resource_eks_cluster.go
+++ rafay/resource_eks_cluster.go
@@ -2672,8 +2672,13 @@ func expandNodeGroups(p []interface{}) []*NodeGroup { //not completed have quest
 		}
 		//doc does not have fields custom ami, enable detailed monitoring, or is wavlength zone but NodeGroupbase struct does
 
-		if v, ok := in["enable_detailed_monitoring"].(bool); ok {
+		if v, ok := in["enable_detailed_monitoring"].(bool); ok && v {
 			obj.EnableDetailedMonitoring = &v
+		} else {
+			// XXX TODO a temporary fix to make TF work with
+			// 1.10 AirGap controller
+			// remove this in future
+			obj.EnableDetailedMonitoring = nil
 		}
 		if v, ok := in["instances_distribution"].([]interface{}); ok && len(v) > 0 {
 			obj.InstancesDistribution = expandNodeGroupInstanceDistribution(v)

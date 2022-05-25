8abc0336696dc61a1b3abd385bb8a7f032b6a9f4
Hardik Italia <hardik@rafay.co>
2022-05-24 18:07:31 -0700

    Updated examples for repo & agent

--- examples/resources/rafay_agent/resource.tf
+++ examples/resources/rafay_agent/resource.tf
@@ -1,13 +1,27 @@
+#Kubernetes agent
+
 resource "rafay_agent" "tfdemoagent1" {
   metadata {
     name    = "tfdemoagent1"
-    project = "upgrade"
+    project = "terraform"
   }
   spec {
         type = "ClusterAgent"
         cluster {
             name = "dev-test"
         }
+        active = true
+  }
+}
+
+#Docker agent
+resource "rafay_agent" "tfdemoagent2" {
+  metadata {
+    name    = "tfdemoagent2"
+    project = "terraform"
+  }
+  spec {
+        type = "Docker"
         active = true
   }
 }
--- examples/resources/rafay_repository/resource.tf
+++ examples/resources/rafay_repository/resource.tf
@@ -3,19 +3,16 @@
 resource "rafay_repositories" "tfdemorepository1" {
   metadata {
     name    = "tfdemorepository1"
-    project = "upgrade"
+    project = "terraform"
   }
 
   spec {
     type = "Git"
     credentials {
-      password = "sealed://credentials.password"
-      username = "hardik"
+      password = "password_token"
+      username = "userone"
     }
-    endpoint = "https://hardik0060@dev.azure.com/hardik0060/gitops/_git/gitops"
-    secret {
-      name = "file://artifacts/tfdemorepository1/sealed-secret.yaml"
-    }
+    endpoint = "https://github.com/test/apps.git"
   }
 }
 
@@ -28,64 +25,25 @@ resource "rafay_repositories" "tfdemorepository2" {
 
   spec {
     type     = "Git"
-    endpoint = "https://github.com/hardik-rafay/apps.git"
+    endpoint = "git@github.com/test/apps.git"
     credentials {
-      password = "sealed://credentials.password"
-      username = "hardik"
+      private_key = file("key_file")
     }
-
-    secret {
-      name = "file://artifacts/tfdemorepository1/sealed-secret.yaml"
-    }
-
-    options {
-      max_retires = 1
-      ca_cert {
-        name = "file://artifacts/tfdemorepository2/ca-cert.pem"
-      }
-    }
-
     agents {
-      name = "aganet-v1.10"
+      name = "gitops-agent"
     }
   }
 }
 
-# Privatekey example
+# Public Helm repo example
 resource "rafay_repositories" "tfdemorepository3" {
   metadata {
     name    = "tfdemorepository3"
-    project = "upgrade"
-  }
-  spec {
-    type     = "Helm"
-    endpoint = "https://aws.github.io/eks-charts"
-    options {
-      max_retires = 1
-    }
-    credentials {
-      private_key = "sealed://credentials.privateKey"
-    }
-    secret {
-      name = "file://artifacts/tfdemorepository3/sealed-secret.yaml"
-    }
-  }
-}
-
-
-# No credentials example
-resource "rafay_repositories" "tfdemorepository4" {
-  metadata {
-    name    = "tfdemorepository4"
-    project = "upgrade"
+    project = "terraform"
   }
 
   spec {
     type     = "Helm"
     endpoint = "https://charts.bitnami.com/bitnami"
-    options {
-      max_retires = 1
-    }
   }
 }
- 

b69aeb6c8887275038da83c6d32cdd462103e225
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-05-26 06:52:03 -0700

    Added missing files

---
+++ rafay/resource_agent.go
@@ -0,0 +1,352 @@
+package rafay
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/RafaySystems/rafay-common/pkg/hub/client/options"
+	typed "github.com/RafaySystems/rafay-common/pkg/hub/client/typed"
+	"github.com/RafaySystems/rafay-common/pkg/hub/terraform/resource"
+	"github.com/RafaySystems/rafay-common/proto/types/hub/gitopspb"
+	"github.com/RafaySystems/rctl/pkg/agent"
+	"github.com/RafaySystems/rctl/pkg/config"
+	"github.com/RafaySystems/rctl/pkg/versioninfo"
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceAgent() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAgentCreate,
+		ReadContext:   resourceAgentRead,
+		UpdateContext: resourceAgentUpdate,
+		DeleteContext: resourceAgentDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		SchemaVersion: 1,
+		Schema:        resource.AgentSchema.Schema,
+	}
+}
+
+func resourceAgentCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("agent create starts")
+	diags := resourceAgentUpsert(ctx, d, m)
+	if diags.HasError() {
+		tflog := os.Getenv("TF_LOG")
+		if tflog == "TRACE" || tflog == "DEBUG" {
+			ctx = context.WithValue(ctx, "debug", "true")
+		}
+		log.Printf("agent create got error, perform cleanup")
+		ag, err := expandAgent(d)
+		if err != nil {
+			log.Printf("agent expandAgent error")
+			return diags
+		}
+		auth := config.GetConfig().GetAppAuthProfile()
+		client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+		if err != nil {
+			return diags
+		}
+
+		err = client.GitopsV3().Agent().Delete(ctx, options.DeleteOptions{
+			Name:    ag.Metadata.Name,
+			Project: ag.Metadata.Project,
+		})
+		if err != nil {
+			return diags
+		}
+	}
+	return diags
+}
+
+func resourceAgentUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("agent update starts")
+	return resourceAgentUpsert(ctx, d, m)
+}
+
+func resourceAgentUpsert(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	log.Printf("agent upsert starts")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	agent, err := expandAgent(d)
+	if err != nil {
+		log.Printf("agent expandAgent error")
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.GitopsV3().Agent().Apply(ctx, agent, options.ApplyOptions{})
+	if err != nil {
+		// XXX Debug
+		n1 := spew.Sprintf("%+v", agent)
+		log.Println("agent apply agent:", n1)
+		log.Printf("agent apply error")
+		return diag.FromErr(err)
+	}
+
+	d.SetId(agent.Metadata.Name)
+	return diags
+
+}
+
+func resourceAgentRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	log.Println("resourceAgentRead ")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+	tfAgentState, err := expandAgent(d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// XXX Debug
+	// w1 := spew.Sprintf("%+v", tfAgentState)
+	// log.Println("resourceAgentRead tfAgentState", w1)
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	ag, err := client.GitopsV3().Agent().Get(ctx, options.GetOptions{
+		Name:    tfAgentState.Metadata.Name,
+		Project: tfAgentState.Metadata.Project,
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// XXX Debug
+	// w1 = spew.Sprintf("%+v", wl)
+	// log.Println("resourceAgentRead wl", w1)
+
+	err = flattenAgent(d, ag)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return diags
+
+}
+
+func resourceAgentDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	ag, err := expandAgent(d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.GitopsV3().Agent().Delete(ctx, options.DeleteOptions{
+		Name:    ag.Metadata.Name,
+		Project: ag.Metadata.Project,
+	})
+
+	if err != nil {
+		//v3 spec gave error try v2
+		return resourceAgentV2Delete(ctx, ag)
+	}
+
+	return diags
+}
+
+func resourceAgentV2Delete(ctx context.Context, ag *gitopspb.Agent) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	projectId, err := config.GetProjectIdByName(ag.Metadata.Project)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	//delete agent
+	err = agent.DeleteAgent(ag.Metadata.Name, projectId)
+	if err != nil {
+		log.Println("error deleting agent")
+	} else {
+		log.Println("Deleted agent: ", ag.Metadata.Name)
+	}
+	return diags
+}
+
+func expandAgent(in *schema.ResourceData) (*gitopspb.Agent, error) {
+	if in == nil {
+		return nil, fmt.Errorf("%s", "expand agent empty input")
+	}
+	obj := &gitopspb.Agent{}
+
+	if v, ok := in.Get("metadata").([]interface{}); ok && len(v) > 0 {
+		obj.Metadata = expandMetaData(v)
+	}
+
+	if v, ok := in.Get("spec").([]interface{}); ok && len(v) > 0 {
+		objSpec, err := expandAgentSpec(v)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("expandAgentSpec got spec")
+		obj.Spec = objSpec
+	}
+
+	obj.ApiVersion = "gitops.k8smgmt.io/v3"
+	obj.Kind = "Agent"
+	return obj, nil
+}
+
+func expandAgentSpec(p []interface{}) (*gitopspb.AgentSpec, error) {
+	obj := &gitopspb.AgentSpec{}
+	if len(p) == 0 || p[0] == nil {
+		return obj, fmt.Errorf("%s", "expandAgentSpec empty input")
+	}
+
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["type"].(string); ok && len(v) > 0 {
+		obj.Type = v
+	}
+
+	if v, ok := in["cluster"].([]interface{}); ok && len(v) > 0 {
+		obj.Cluster = expandAgentClusterMeta(v)
+	}
+
+	if v, ok := in["active"].(bool); ok {
+		obj.Active = v
+	}
+
+	return obj, nil
+}
+
+func expandAgentClusterMeta(p []interface{}) *gitopspb.ClusterMeta {
+	obj := &gitopspb.ClusterMeta{}
+	if len(p) == 0 || p[0] == nil {
+		return obj
+	}
+
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["name"].(string); ok && len(v) > 0 {
+		obj.Name = v
+	}
+
+	if v, ok := in["id"].(string); ok && len(v) > 0 {
+		obj.Id = v
+	}
+
+	return obj
+
+}
+
+// Flatteners
+
+func flattenAgent(d *schema.ResourceData, in *gitopspb.Agent) error {
+	if in == nil {
+		return nil
+	}
+
+	err := d.Set("metadata", flattenMetaData(in.Metadata))
+	if err != nil {
+		return err
+	}
+
+	v, ok := d.Get("spec").([]interface{})
+	if !ok {
+		v = []interface{}{}
+	}
+
+	// XXX Debug
+	// w1 := spew.Sprintf("%+v", v)
+	// log.Println("flattenAgent before ", w1)
+	var ret []interface{}
+	ret, err = flattenAgentSpec(in.Spec, v)
+	if err != nil {
+		return err
+	}
+	// XXX Debug
+	// w1 = spew.Sprintf("%+v", ret)
+	// log.Println("flattenAgent after ", w1)
+
+	err = d.Set("spec", ret)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func flattenAgentSpec(in *gitopspb.AgentSpec, p []interface{}) ([]interface{}, error) {
+	if in == nil {
+		return nil, fmt.Errorf("%s", "flattenAgentSpec empty input")
+	}
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if len(in.Type) > 0 {
+		obj["type"] = in.Type
+	}
+
+	if in.Cluster != nil {
+		v, ok := obj["cluster"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["cluster"] = flattenAgentClusterMeta(in.Cluster, v)
+	}
+
+	obj["active"] = in.Active
+
+	return []interface{}{obj}, nil
+}
+
+func flattenAgentClusterMeta(in *gitopspb.ClusterMeta, p []interface{}) []interface{} {
+	if in == nil {
+		return nil
+	}
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if len(in.Name) > 0 {
+		obj["name"] = in.Name
+	}
+
+	if len(in.Id) > 0 {
+		obj["id"] = in.Id
+	}
+
+	return []interface{}{obj}
+}
---
+++ rafay/resource_repositories.go
@@ -0,0 +1,558 @@
+package rafay
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/RafaySystems/rafay-common/pkg/hub/client/options"
+	typed "github.com/RafaySystems/rafay-common/pkg/hub/client/typed"
+	"github.com/RafaySystems/rafay-common/pkg/hub/terraform/resource"
+	"github.com/RafaySystems/rafay-common/proto/types/hub/commonpb"
+	"github.com/RafaySystems/rafay-common/proto/types/hub/integrationspb"
+	"github.com/RafaySystems/rctl/pkg/config"
+	"github.com/RafaySystems/rctl/pkg/versioninfo"
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type repositorySpec struct {
+	Type        string                            `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`
+	Endpoint    string                            `protobuf:"bytes,2,opt,name=endpoint,proto3" json:"endpoint,omitempty"`
+	Agents      []*integrationspb.AgentMeta       `protobuf:"bytes,3,rep,name=agents,proto3" json:"agents,omitempty"`
+	Options     *integrationspb.RepositoryOptions `protobuf:"bytes,4,opt,name=options,proto3" json:"options,omitempty"`
+	Secret      *commonpb.File                    `protobuf:"bytes,5,opt,name=secret,proto3" json:"secret,omitempty"`
+	Credentials struct {
+		Username   string `protobuf:"bytes,1,opt,name=username,proto3" json:"username,omitempty"`
+		Password   string `protobuf:"bytes,2,opt,name=password,proto3" json:"password,omitempty"`
+		PrivateKey string `protobuf:"bytes,1,opt,name=privateKey,proto3" json:"privateKey,omitempty"`
+	} `json:"credentials,omitempty"`
+}
+
+func resourceRepositories() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceRepositoriesCreate,
+		ReadContext:   resourceRepositoriesRead,
+		UpdateContext: resourceRepositoriesUpdate,
+		DeleteContext: resourceRepositoriesDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(10 * time.Minute),
+			Update: schema.DefaultTimeout(10 * time.Minute),
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		SchemaVersion: 1,
+		Schema:        resource.RepositorySchema.Schema,
+	}
+}
+
+func resourceRepositoriesCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("repository create starts")
+	return resourceRepositoryUpsert(ctx, d, m)
+}
+
+func resourceRepositoryUpsert(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	log.Printf("repository upsert starts")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	repo, err := expandRepository(d)
+	if err != nil {
+		log.Printf("repository expandRepository error")
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.IntegrationsV3().Repository().Apply(ctx, repo, options.ApplyOptions{})
+	if err != nil {
+		// XXX Debug
+		n1 := spew.Sprintf("%+v", repo)
+		log.Println("repository apply repository:", n1)
+		log.Printf("repository apply error")
+		return diag.FromErr(err)
+	}
+
+	d.SetId(repo.Metadata.Name)
+	return diags
+}
+
+func resourceRepositoriesRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	log.Printf("repository read starts")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	repoTFState, err := expandRepository(d)
+	if err != nil {
+		log.Printf("repository expandRepository error")
+		return diag.FromErr(err)
+	}
+
+	// XXX Debug
+	w1 := spew.Sprintf("%+v", repoTFState)
+	log.Println("resourceRepositoriesRead repoTFState", w1)
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	repo, err := client.IntegrationsV3().Repository().Get(ctx, options.GetOptions{
+		Name:    repoTFState.Metadata.Name,
+		Project: repoTFState.Metadata.Project,
+	})
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	// XXX Debug
+	// w1 = spew.Sprintf("%+v", repo)
+	// log.Println("resourceRepositoriesRead wl", w1)
+
+	err = flattenRepository(d, repo)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	return diags
+}
+
+func resourceRepositoriesUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	log.Printf("repository update starts")
+	return resourceRepositoryUpsert(ctx, d, m)
+}
+
+func resourceRepositoriesDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	log.Printf("repository upsert starts")
+	tflog := os.Getenv("TF_LOG")
+	if tflog == "TRACE" || tflog == "DEBUG" {
+		ctx = context.WithValue(ctx, "debug", "true")
+	}
+
+	repo, err := expandRepository(d)
+	if err != nil {
+		log.Printf("repository expandRepository error")
+		return diag.FromErr(err)
+	}
+
+	auth := config.GetConfig().GetAppAuthProfile()
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.IntegrationsV3().Repository().Delete(ctx, options.DeleteOptions{
+		Name:    repo.Metadata.Name,
+		Project: repo.Metadata.Project,
+	})
+	if err != nil {
+		// XXX Debug
+		n1 := spew.Sprintf("%+v", repo)
+		log.Println("repository delete repository:", n1)
+		log.Printf("repository delete error")
+		return diag.FromErr(err)
+	}
+	return diags
+}
+
+// Expanders
+
+func expandRepository(in *schema.ResourceData) (*integrationspb.Repository, error) {
+	if in == nil {
+		return nil, fmt.Errorf("%s", "expand blueprint empty input")
+	}
+	obj := &integrationspb.Repository{}
+
+	if v, ok := in.Get("metadata").([]interface{}); ok && len(v) > 0 {
+		obj.Metadata = expandMetaData(v)
+	}
+
+	if v, ok := in.Get("spec").([]interface{}); ok && len(v) > 0 {
+		objSpec, err := expandRepositorySpec(v)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("expandRepositorySpec got spec")
+		obj.Spec = objSpec
+	}
+
+	obj.ApiVersion = "integrations.k8smgmt.io/v3"
+	obj.Kind = "Repository"
+	return obj, nil
+}
+
+func expandRepositorySpec(p []interface{}) (*integrationspb.RepositorySpec, error) {
+	repoSpec := repositorySpec{}
+	obj := integrationspb.RepositorySpec{}
+
+	if len(p) == 0 || p[0] == nil {
+		return &obj, fmt.Errorf("%s", "expandAddonSpec empty input")
+	}
+
+	in := p[0].(map[string]interface{})
+
+	if v, ok := in["type"].(string); ok && len(v) > 0 {
+		//obj.Type = v
+		repoSpec.Type = v
+	}
+
+	if v, ok := in["endpoint"].(string); ok && len(v) > 0 {
+		//obj.Endpoint = v
+		repoSpec.Endpoint = v
+	}
+
+	if v, ok := in["agents"].([]interface{}); ok && len(v) > 0 {
+		//obj.Agents = expandAgents(v)
+		repoSpec.Agents = expandAgents(v)
+	}
+
+	if v, ok := in["options"].([]interface{}); ok && len(v) > 0 {
+		//obj.Options = expandRepositoryOptions(v)
+		repoSpec.Options = expandRepositoryOptions(v)
+	} else {
+		repoSpec.Options = expandRepositoryOptions(nil)
+	}
+
+	if v, ok := in["secret"].([]interface{}); ok {
+		//obj.Secret = expandCommonpbFile(v)
+		repoSpec.Secret = expandCommonpbFile(v)
+	}
+
+	if vp, ok := in["credentials"].([]interface{}); ok && len(vp) > 0 {
+		if len(vp) == 0 || vp[0] == nil {
+			return nil, fmt.Errorf("%s", "expandRepoCredential empty ")
+		}
+		inp := vp[0].(map[string]interface{})
+		if v, ok := inp["username"].(string); ok && len(v) > 0 {
+			repoSpec.Credentials.Username = v
+		}
+
+		if v, ok := inp["password"].(string); ok && len(v) > 0 {
+			repoSpec.Credentials.Password = v
+		}
+
+		if v, ok := inp["private_key"].(string); ok && len(v) > 0 {
+			repoSpec.Credentials.PrivateKey = v
+		}
+	}
+
+	// XXX Debug
+	s := spew.Sprintf("%+v", repoSpec)
+	log.Println("expandRepositorySpec repoSpec", s)
+
+	jsonSpec, err := json.Marshal(repoSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	// XXX Debug
+	log.Println("expandRepositorySpec jsonSpec ", string(jsonSpec))
+
+	err = obj.UnmarshalJSON(jsonSpec)
+	if err != nil {
+		log.Println("expandRepositorySpec UnmarshalJSON error ", err)
+		return nil, err
+	}
+
+	// XXX Debug
+	s1 := spew.Sprintf("%+v", obj)
+	log.Println("expandRepositorySpec obj", s1)
+
+	return &obj, nil
+}
+
+func expandRepositoryOptions(p []interface{}) *integrationspb.RepositoryOptions {
+	obj := &integrationspb.RepositoryOptions{}
+	if len(p) == 0 || p[0] == nil {
+		return obj
+	}
+
+	in := p[0].(map[string]interface{})
+	if v, ok := in["insecure"].(bool); ok {
+		obj.Insecure = v
+	} else {
+		obj.Insecure = false
+	}
+
+	if v, ok := in["enable_submodules"].(bool); ok {
+		obj.EnableSubmodules = v
+	} else {
+		obj.EnableSubmodules = false
+	}
+
+	if v, ok := in["max_retires"].(int); ok {
+		obj.MaxRetires = int32(v)
+	} else {
+		obj.MaxRetires = 0
+	}
+
+	if v, ok := in["enable_lfs"].(bool); ok {
+		obj.EnableLFS = v
+	} else {
+		obj.EnableLFS = false
+	}
+
+	if v, ok := in["ca_cert"].([]interface{}); ok {
+		obj.CaCert = expandCommonpbFile(v)
+	}
+
+	return obj
+}
+
+// Flatteners
+
+func flattenAgents(input []*integrationspb.AgentMeta, p []interface{}) []interface{} {
+	log.Println("flattenAgents")
+	if input == nil {
+		return nil
+	}
+
+	out := make([]interface{}, len(input))
+	for i, in := range input {
+		log.Println("flattenAgents in ", in)
+		obj := map[string]interface{}{}
+		if i < len(p) && p[i] != nil {
+			obj = p[i].(map[string]interface{})
+		}
+
+		if len(in.Name) > 0 {
+			obj["name"] = in.Name
+		}
+
+		// if len(in.Name) > 0 {
+		// 	obj["id"] = in.Id
+		// }
+
+		out[i] = &obj
+	}
+
+	return out
+}
+
+func flattenCredentials(in *repositorySpec, p []interface{}) []interface{} {
+	if in == nil {
+		return nil
+	}
+
+	if len(p) == 0 || p[0] == nil {
+		return nil
+	}
+
+	obj := p[0].(map[string]interface{})
+
+	if len(in.Credentials.Username) > 0 {
+		obj["username"] = in.Credentials.Username
+	}
+
+	if len(in.Credentials.Password) > 0 {
+		obj["password"] = in.Credentials.Password
+	}
+
+	if len(in.Credentials.PrivateKey) > 0 {
+		obj["private_key"] = in.Credentials.PrivateKey
+	}
+
+	return []interface{}{obj}
+}
+
+func flattenRepoOptions(in *integrationspb.RepositoryOptions, p []interface{}) []interface{} {
+	if in == nil {
+		return nil
+	}
+
+	retNel := true
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if in.Insecure {
+		obj["insecure"] = in.Insecure
+		retNel = false
+	} else {
+		if v1, ok := obj["insecure"].(bool); ok && v1 {
+			obj["insecure"] = false
+			retNel = false
+		}
+	}
+
+	if in.EnableSubmodules {
+		obj["enable_submodules"] = in.EnableSubmodules
+		retNel = false
+	} else {
+		if v1, ok := obj["enable_submodules"].(bool); ok && v1 {
+			obj["enable_submodules"] = false
+			retNel = false
+		}
+	}
+
+	if in.MaxRetires > 0 {
+		obj["max_retires"] = int(in.MaxRetires)
+		retNel = false
+	} else {
+		if v1, ok := obj["max_retires"].(int); ok && v1 != int(in.MaxRetires) {
+			obj["max_retires"] = 0
+			retNel = false
+		}
+	}
+
+	if in.EnableLFS {
+		obj["enable_lfs"] = in.EnableLFS
+		retNel = false
+	} else {
+		if v1, ok := obj["enable_lfs"].(bool); ok && v1 {
+			obj["enable_lfs"] = false
+			retNel = false
+		}
+	}
+
+	// if in.CaCert != nil {
+	// 	obj["ca_cert"] = flattenCommonpbFile(in.CaCert)
+	// 	retNel = false
+	// }
+
+	if retNel {
+		return nil
+	}
+
+	return []interface{}{obj}
+}
+
+// flattenRepositorySpec RepositorySpec to TF State
+func flattenRepositorySpec(in *integrationspb.RepositorySpec, p []interface{}) ([]interface{}, error) {
+	if in == nil {
+		log.Println("flattenRepositorySpec empty input")
+		return nil, fmt.Errorf("%s", "flattenRepositorySpec empty input")
+	}
+
+	// XXX Debug
+	in1 := spew.Sprintf("%+v", in)
+	log.Println("flattenRepositorySpec in", in1)
+
+	// XXX Debug
+	ob := spew.Sprintf("%+v", p)
+	log.Println("flattenRepositorySpec p", ob)
+
+	obj := map[string]interface{}{}
+	if len(p) != 0 && p[0] != nil {
+		obj = p[0].(map[string]interface{})
+	}
+
+	if len(in.Type) > 0 {
+		obj["type"] = in.Type
+	} else {
+		obj["type"] = ""
+	}
+
+	if len(in.Endpoint) > 0 {
+		obj["endpoint"] = in.Endpoint
+	} else {
+		obj["endpoint"] = ""
+	}
+
+	if len(in.Agents) > 0 {
+		v, ok := obj["agents"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+
+		obj["agents"] = flattenAgents(in.Agents, v)
+	} else {
+		obj["agents"] = nil
+	}
+
+	if in.Options != nil {
+		v, ok := obj["options"].([]interface{})
+		if !ok {
+			v = []interface{}{}
+		}
+		obj["options"] = flattenRepoOptions(in.Options, v)
+	} else {
+		obj["options"] = nil
+	}
+
+	if in.Secret != nil {
+		obj["secrets"] = flattenCommonpbFile(in.Secret)
+	}
+
+	jsonBytes, err := in.MarshalJSON()
+	if err != nil {
+		log.Println("flattenRepositorySpec MarshalJSON error", err)
+		return nil, fmt.Errorf("%s %+v", "flattenRepositorySpec MarshalJSON error", err)
+	}
+
+	rs := repositorySpec{}
+	err = json.Unmarshal(jsonBytes, &rs)
+	if err != nil {
+		return nil, fmt.Errorf("%s %+v", "flattenRepositorySpec json unmarshal error", err)
+	}
+
+	// XXX Debug
+	log.Println("flattenRepositorySpec jsonBytes:", string(jsonBytes))
+	s1 := spew.Sprintf("%+v", rs)
+	log.Println("flattenRepositorySpec rs", s1)
+
+	v, ok := obj["credentials"].([]interface{})
+	if !ok {
+		v = []interface{}{}
+	}
+	obj["credentials"] = flattenCredentials(&rs, v)
+
+	// XXX Debug
+	o1 := spew.Sprintf("%+v", obj)
+	log.Println("flattenRepositorySpec obj", o1)
+
+	return []interface{}{obj}, nil
+}
+
+func flattenRepository(d *schema.ResourceData, in *integrationspb.Repository) error {
+	if in == nil {
+		return nil
+	}
+
+	err := d.Set("metadata", flattenMetaData(in.Metadata))
+	if err != nil {
+		return err
+	}
+
+	v, ok := d.Get("spec").([]interface{})
+	if !ok {
+		v = []interface{}{}
+	}
+
+	// XXX Debug
+	w1 := spew.Sprintf("%+v", v)
+	log.Println("flattenRepository before ", w1)
+	var ret []interface{}
+	ret, err = flattenRepositorySpec(in.Spec, v)
+	if err != nil {
+		return err
+	}
+	// XXX Debug
+	w1 = spew.Sprintf("%+v", ret)
+	log.Println("flattenRepository after ", w1)
+
+	err = d.Set("spec", ret)
+	if err != nil {
+		return err
+	}
+	return nil
+}

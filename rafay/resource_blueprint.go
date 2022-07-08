1fefea05a4af70a7edda1edcb83f5e9dbb131add
Srikar <102141644+saisrikar-rafay@users.noreply.github.com>
2022-07-08 12:05:58 +0530

    Revert "RC-14598 - Golden base blueprint"

--- rafay/resource_blueprint.go
+++ rafay/resource_blueprint.go
@@ -266,10 +266,6 @@ func expandBluePrintSpec(p []interface{}) (*infrapb.BlueprintSpec, error) {
 		obj.Sharing = expandSharingSpec(v)
 	}
 
-	if v, ok := in["type"].(string); ok && len(v) > 0 {
-		obj.Type = v
-	}
-
 	if v, ok := in["drift"].([]interface{}); ok && len(v) > 0 {
 		obj.Drift = expandDrift(v)
 	}

84f06b936d021eb0f3718d72b4dbf2727212fb2a
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-08-08 13:28:29 -0700

    renamed rafay_d_kube_config to rafay_download_kubeconfig

--- examples/resources/rafay_d_kube_config/output.tf
+++
@@ -1,3 +0,0 @@
-output "kubeconfig" {
-  value = rafay_d_kube_config.tfkubeconfig1
-}
--- examples/resources/rafay_d_kube_config/resource.tf
+++
@@ -1,6 +0,0 @@
-resource "rafay_d_kube_config" "tfkubeconfig1" {
-  namespace = "vault"
-  cluster = "hardik-pwc-test"
-  output_folder_path = ""
-  filename = "kc-testfile"
-}

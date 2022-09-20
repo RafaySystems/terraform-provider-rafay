569b0766a781f76b083c8fef42727c0715c084ac
Gopi M <gopi@Rafays-MacBook-Pro.local>
2022-09-20 22:45:00 +0530

    RC-8912: Revert example

--- examples/resources/rafay_project/resource.tf
+++ examples/resources/rafay_project/resource.tf
@@ -9,94 +9,43 @@ resource "rafay_project" "tfdemoproject1" {
   }
 }
 
-resource "rafay_eks_cluster" "ekscluster-basic" {
-  cluster {
-    kind = "Cluster"
-    metadata {
-      name    = "eks-cluster-1"
-      project = "defaultproject"
-    }
-    spec {
-      type           = "eks"
-      blueprint      = "default"
-      blueprint_version = "1.12.0"
-      cloud_provider = "gopi-aws"
-      cni_provider   = "aws-cni"
-      proxy_config   = {}
-    }
+# Project with resource quota
+resource "rafay_project" "tfdemoproject2" {
+  metadata {
+    name        = "terraform-quota"
+    description = "terraform quota project"
   }
-  cluster_config {
-    apiversion = "rafay.io/v1alpha5"
-    kind       = "ClusterConfig"
-    metadata {
-      name    = "eks-cluster-1"
-      region  = "us-west-2"
-      version = "1.21"
+  spec {
+    default = false
+    cluster_resource_quota {
+      cpu_requests = "8m"
+      memory_requests = "4Mi"
+      cpu_limits = "6m"
+      memory_limits = "8Mi"
+      config_maps = "10"
+      persistent_volume_claims = "5"
+      secrets = "4"
+      services = "20"    
+      pods = "200"
+      replication_controllers = "10"
+      services_load_balancers = "3"
+      services_node_ports = "10"
+      storage_requests = "10Gi"
     }
-    iam {
-      service_accounts {
-        attach_policy = <<EOF
-        {
-          "Version": "2012-10-17",
-          "Statement": [
-            {
-              "Effect": "Allow",
-              "Action": "ec2:Describe*",
-              "Resource": "*"
-            },
-            {
-              "Effect": "Allow",
-              "Action": "ec2:AttachVolume",
-              "Resource": "*"
-            },
-            {
-              "Effect": "Allow",
-              "Action": "ec2:DetachVolume",
-              "Resource": "*"
-            },
-            {
-              "Effect": "Allow",
-              "Action": ["ec2:*"],
-              "Resource": ["*"]
-            },
-            {
-              "Effect": "Allow",
-              "Action": ["elasticloadbalancing:*"],
-              "Resource": ["*"]
-            }
-          ]
-        }
-        EOF
-      }
-    }
-    vpc {
-      cidr = "192.168.0.0/16"
-      cluster_endpoints {
-        private_access = true
-        public_access  = false
-      }
-      nat {
-        gateway = "Single"
-      }
-    }
-    node_groups {
-      name       = "ng-1"
-      ami_family = "AmazonLinux2"
-      iam {
-        iam_node_group_with_addon_policies {
-          image_builder = true
-          auto_scaler   = true
-        }
-      }
-      instance_type    = "t3.2xlarge"
-      desired_capacity = 1
-      min_size         = 1
-      max_size         = 2
-      max_pods_per_node = 50
-      version          = "1.21"
-      volume_size      = 80
-      volume_type      = "gp3"
-      private_networking = true
+    default_cluster_namespace_quota {
+      cpu_requests = "4m"
+      memory_requests = "2Mi"
+      cpu_limits = "2m"
+      memory_limits = "4Mi"
+      config_maps = "5"
+      persistent_volume_claims = "2"
+      secrets = "2"
+      services = "10"
+      pods = "20"
+      replication_controllers = "4"
+      services_load_balancers = "3"
+      services_node_ports = "10"
+      storage_requests = "10Gi"
     }
   }
 }

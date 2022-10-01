75bc4b76732ced6c777bea46b5b89f74e1587af9
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-10-01 22:33:36 +0530

    Fix namespace limit range diff issues

--- examples/resources/rafay_namespace/resource.tf
+++ examples/resources/rafay_namespace/resource.tf
@@ -20,79 +20,79 @@ resource "rafay_namespace" "tfdemonamespace1" {
 
 #Namespace example with resource quotas & limit ranges
 resource "rafay_namespace" "namespace" {
-  metadata {
-    name    = "cloudops"
-    project = "terraform"
-    labels = {
-      "env" = "prod"
+    metadata {
+        name    = "cloudops"
+        project = "terraform"
+        labels = {
+            "env" = "prod"
+        }
+        annotations = {
+            "logging" = "enabled"
+        }
     }
-    annotations = {
-      "logging" = "enabled"
-    }
-  }
-  spec {
-    drift {
-      enabled = false
-    }
-    placement {
-      labels {
-        key   = "rafay.dev/clusterName"
-        value = "cluster_name"
-      }
-    }
-    limit_range {
-      pod {
-        max {
-          cpu  = "2"
-          memory = "2Gi"
+    spec {
+        drift {
+            enabled = false
         }
-        min {
-          cpu  = "1"
-          memory = "1Gi"
+        placement {
+            labels {
+            key   = "rafay.dev/clusterName"
+            value = "cluster_name"
+            }
         }
-        ratio {
-          cpu    = 1
-          memory = 1
-        }
-      }
-      container {
-        default {
-          cpu = "1"   
-          memory = "1Gi"
-        }
-        default_request {
-          cpu = "1"
-          memory = "1Gi"
-        }
+        limit_range {
+            pod {
+                max {
+                    cpu  = "500m"
+                    memory = "128Mi"
+                }
+                min {
+                    cpu  = "250m"
+                    memory = "64Mi"
+                }
+                ratio {
+                    cpu    = 1
+                    memory = 1
+                }
+            }
+            container {
+                default {
+                    cpu  = "250m"
+                    memory = "64Mi"
+                }
+                default_request {
+                    cpu  = "250m"
+                    memory = "64Mi"
+                }
 
-        max {
-          cpu = "2"
-          memory = "2Gi"
+                max {
+                    cpu  = "500m"
+                    memory = "128Mi"
+                }
+                min {
+                    cpu  = "250m"
+                    memory = "64Mi"
+                }
+                ratio {
+                    cpu    = 1
+                    memory = 1
+                }
+            }
         }
-        min {
-          cpu = "1"   
-          memory = "2Gi"
+        resource_quotas {
+            config_maps = "10"
+            cpu_limits = "8"
+            memory_limits = "16Gi"
+            cpu_requests = "4"
+            memory_requests = "8Gi"
+            persistent_volume_claims = "2"
+            pods = "30"
+            replication_controllers = "5"
+            secrets = "10"
+            services = "10"
+            services_load_balancers = "3"
+            services_node_ports = "10"
+            storage_requests = "10737418240"
         }
-        ratio {
-          cpu    = 1
-          memory = 1
-        }
-      }
-    }
-    resource_quotas {
-      config_maps = "10"
-      cpu_limits = "8"
-      memory_limits = "16Gi"
-      cpu_requests = "4"
-      memory_requests = "8Gi"
-      persistent_volume_claims = "2"
-      pods = "30"
-      replication_controllers = "5"
-      secrets = "10"
-      services = "10"
-      services_load_balancers = "3"
-      services_node_ports = "10"
-      storage_requests = "10Gi"
-      }
     }
 }

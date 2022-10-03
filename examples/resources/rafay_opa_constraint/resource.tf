3f8db49f17b880f77beee3d4731af3918f94b5a3
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-10-04 00:51:11 +0530

    updated opa example

--- examples/resources/rafay_opa_constraint/resource.tf
+++ examples/resources/rafay_opa_constraint/resource.tf
@@ -2,16 +2,26 @@
 resource "rafay_opa_constraint" "tfdemoopaconstraint1" {
   metadata {
     name    = "tfdemoopaconstraint1"
-    project = "tfdemoproject1"
+    project     = "tfdemoproject1"
+    labels = {
+      "rafay.dev/opa" = "constraint"
+    }
   }
   spec {
     artifact {
+      type = "Yaml"
       artifact {
         paths {
-          name = "file://artifacts/one/request-limit-ratio-constraint.yaml"
+          name = "file://artifacts/request-limit-ratio/request-limit-ratio.yaml"
         }
       }
-      template_name = "one"
+      options {
+      	force =  true
+    	disable_open_api_validation = true
+      }
     }
+    template_name = "k8srequiredlabels"
+    version = "v1"
+    published =  true
   }
 }

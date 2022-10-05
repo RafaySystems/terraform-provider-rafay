ecc7319b69637d21900093800df55855ce5b1464
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-10-05 18:16:02 +0530

    Update resource.tf

--- examples/resources/rafay_user/resource.tf
+++ examples/resources/rafay_user/resource.tf
@@ -2,13 +2,14 @@ resource "rafay_user" "user" {
   user_name = "sampleUser"
   first_name = "Bob"
   last_name = "Ross"
-  phone = ""
-  groups = ["group1-InfraAdmin"]
+  phone = "14083074010"
+  groups = ["group1", "group2"]
   generate_apikey = true
+  console_access = true
 }
 
 output "apikey" {
   description = "user api key"
   sensitive = true
-  value       = rafay_user.user.apikey
+  value     = rafay_user.user.apikey
 }

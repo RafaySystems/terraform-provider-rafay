f0642fa9256f2082e0b7afbbb1a86c07221117a9
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2022-10-06 17:07:50 +0530

    Cherry pick fixes from master

--- rafay/resource_pipeline.go
+++ rafay/resource_pipeline.go
@@ -475,6 +475,7 @@ func resourcePipelineV2Delete(ctx context.Context, pl *gitopspb.Pipeline) diag.D
 	err = pipeline.DeletePipeline(pl.Metadata.Name, projectId)
 	if err != nil {
 		log.Println("error deleting pipeline")
+		return diag.FromErr(err)
 	} else {
 		log.Println("Deleted pipeline: ", pl.Metadata.Name)
 	}

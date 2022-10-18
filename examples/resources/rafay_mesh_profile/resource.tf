5c51084404088cda28a542fa57f2189dfd142e4b
Sachin Kumar <sachin@rafay.co>
2022-10-18 12:58:11 +0530

    Add blueprint and namespace examples with service mesh

--- examples/resources/rafay_mesh_profile/resource.tf
+++ examples/resources/rafay_mesh_profile/resource.tf
@@ -4,9 +4,29 @@ resource "rafay_mesh_profile" "tfdemomeshprofile1" {
     project = "terraform"
   }
   spec {
-    version = "example-version"
+    version = "v0"
     sharing {
         enabled = false
+    }
+  }
+}
+
+#Example profile with installation params
+resource "rafay_mesh_profile" "tfdemomeshprofile1" {
+  metadata {
+    name    = "tfdemomeshprofile-ip"
+    project = "terraform"
+  }
+  spec {
+    version = "v0"
+    installation_params {
+      cert_type = 0
+      enable_ingress = false
+      enable_namespaces_by_default = false
+      resource_quota {
+        cpu_requests = "500m"
+        memory_requests = "2048Mi"
+      }
     }
   }
 }

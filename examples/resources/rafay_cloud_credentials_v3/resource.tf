3648d43ae52e78888b2049d9352fbec302df6ccd
Chaitanya Angadala <cangadala@rafay.co>
2023-01-13 09:29:16 -0800

    change

--- examples/resources/rafay_cloud_credentials_v3/resource.tf
+++ examples/resources/rafay_cloud_credentials_v3/resource.tf
@@ -2,16 +2,17 @@
 #Basic example for credentials
 resource "rafay_cloud_credentials_v3" "tftestcredentials" {
   metadata {
-    name    = "terraform-demo-credentials-2"
-    project = "cang-test"
+    name    = "terraform-demo-credentials-3"
+    project = "defaultproject"
   }
   spec {
-    type = "CostManagement"
+    type = "ClusterProvisioning"
     provider = "aws"
     credentials {
         type = "AccessKey"
         access_id = "dummy-id"
         secret_key = "dummy-key"
+        session_token = "fake-token"
     } 
     sharing {
       enabled = false

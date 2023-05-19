be7499210e7bbfba5406065812c7e63066d088aa
janardhan-rafay <janardhan@rafay.co>
2023-05-19 21:16:50 +0530

    RC-25886: Terraform support for TagGroup and ProjectTagsAssociation objects

---
+++ examples/resources/rafay_projecttagsassociation/variables.tf
@@ -0,0 +1,5 @@
+variable "rafay_config_file" {
+  description = "rafay provider config file for authentication"
+  sensitive   = true
+  default     = "/Users/janardhan/tmp/rctl/Rafay_nikhil-nikhil@rafay.co.json"
+}

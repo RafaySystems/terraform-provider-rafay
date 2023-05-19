f17a752590c5ffc056f2697e18ecbd0a2918a223
janardhan-rafay <janardhan@rafay.co>
2023-05-19 21:24:42 +0530

    Updated config.json path to be generic path in example files

--- examples/resources/rafay_projecttagsassociation/variables.tf
+++ examples/resources/rafay_projecttagsassociation/variables.tf
@@ -1,5 +1,5 @@
 variable "rafay_config_file" {
   description = "rafay provider config file for authentication"
   sensitive   = true
-  default     = "/Users/janardhan/tmp/rctl/Rafay_nikhil-nikhil@rafay.co.json"
+  default     = "/Users/user1/.rafay/cli/config.json"
 }

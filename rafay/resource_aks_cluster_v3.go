6ee852c3f264169d87803e28cd9698b45f9fa39a
poornima-rafay <106130249+poornima-rafay@users.noreply.github.com>
2023-06-21 12:28:10 -0700

    RC-26132 initial commit of CRUD for GKE tf (#314)
    * RC-26132 initial commit of CRUD for GKE tf
    * initial expanders for gke
    * create gke_flatten_util

--- rafay/resource_aks_cluster_v3.go
+++ rafay/resource_aks_cluster_v3.go
@@ -1868,27 +1868,6 @@ func flattenAKSClusterV3(d *schema.ResourceData, in *infrapb.Cluster) error {
 
 }
 
-func flattenMetadataV3(in *commonpb.Metadata, p []interface{}) []interface{} {
-	if in == nil {
-		return nil
-	}
-	obj := map[string]interface{}{}
-
-	if len(in.Name) > 0 {
-		obj["name"] = in.Name
-	}
-
-	if len(in.Project) > 0 {
-		obj["project"] = in.Project
-	}
-
-	if in.Labels != nil && len(in.Labels) > 0 {
-		obj["labels"] = toMapInterface(in.Labels)
-	}
-
-	return []interface{}{obj}
-}
-
 func flattenClusterV3Spec(in *infrapb.ClusterSpec, p []interface{}) []interface{} {
 	if in == nil {
 		return nil

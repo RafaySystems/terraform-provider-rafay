886ce7c60fd1089839d8da4d8dffa83cd1da42b3
poornima-rafay <106130249+poornima-rafay@users.noreply.github.com>
2023-06-21 12:28:10 -0700

    RC-26132 initial commit of CRUD for GKE tf (#314)
    * RC-26132 initial commit of CRUD for GKE tf
    * initial expanders for gke
    * create gke_flatten_util

---
+++ examples/resources/rafay_gke_cluster_v3/variables.tf
@@ -0,0 +1,5 @@
+variable "rafay_config_file" {
+  description = "rafay provider config file for authentication"
+  sensitive   = true
+  default     = "/Users/poornima.v/Downloads/Rafay_poornima-poornima@rafay.co.json"
+}
---
+++ rafay/cluster_v3_util.go
@@ -0,0 +1,39 @@
+package rafay
+
+import (
+	"github.com/RafaySystems/rafay-common/proto/types/hub/commonpb"
+)
+
+const (
+	V3_CLUSTER_APIVERSION = "infra.k8smgmt.io/v3"
+	V3_CLUSTER_KIND       = "Cluster"
+
+	GKE_CLUSTER_TYPE = "gke"
+
+	GKE_ZONAL_CLUSTER_TYPE    = "zonal"
+	GKE_REGIONAL_CLUSTER_TYPE = "regional"
+
+	GKE_PRIVATE_CLUSTER_TYPE = "private"
+	GKE_PUBLIC_CLUSTER_TYPE  = "public"
+)
+
+func flattenMetadataV3(in *commonpb.Metadata, p []interface{}) []interface{} {
+	if in == nil {
+		return nil
+	}
+	obj := map[string]interface{}{}
+
+	if len(in.Name) > 0 {
+		obj["name"] = in.Name
+	}
+
+	if len(in.Project) > 0 {
+		obj["project"] = in.Project
+	}
+
+	if in.Labels != nil && len(in.Labels) > 0 {
+		obj["labels"] = toMapInterface(in.Labels)
+	}
+
+	return []interface{}{obj}
+}

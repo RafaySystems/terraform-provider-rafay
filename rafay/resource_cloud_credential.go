da1ba53af421e33641a47bae591d7e231a9bcd7d
mayank-rafay <mayank@rafay.co>
2023-07-23 13:21:24 +0530

    fix for mandatory external id in aws/azure credentials

--- rafay/resource_cloud_credential.go
+++ rafay/resource_cloud_credential.go
@@ -142,17 +142,22 @@ func resourceCloudCredentialCreate(ctx context.Context, d *schema.ResourceData, 
 	}
 
 	if d.Get("type").(string) == "cluster-provisioning" || d.Get("type").(string) == "data-backup" {
+		credType := cloudprovider.CredTypeClusterProvisioning
+		if d.Get("type").(string) == "data-backup" {
+			credType = cloudprovider.CredTypeDataBackup
+		}
 		if d.Get("providertype").(string) == "AWS" {
 			if d.Get("awscredtype").(string) == "rolearn" {
 				if d.Get("rolearn").(string) == "" {
 					return diag.FromErr(fmt.Errorf("RoleARN cannot be empty"))
 				}
 				log.Printf("create cloud credential with name %s, %s", d.Get("name").(string), project.ID)
+
 				s, err := cloudprovider.CreateAWSCloudRoleCredentials(
 					d.Get("name").(string),
 					project.ID, d.Get("rolearn").(string),
 					d.Get("externalid").(string),
-					1)
+					credType)
 				if err != nil {
 					log.Printf("create cloud credential error %s", err.Error())
 					return diag.FromErr(err)
@@ -170,7 +175,7 @@ func resourceCloudCredentialCreate(ctx context.Context, d *schema.ResourceData, 
 					project.ID,
 					d.Get("accesskey").(string),
 					d.Get("secretkey").(string), "",
-					1)
+					credType)
 				if err != nil {
 					log.Printf("create cloud credential error %s", err.Error())
 					return diag.FromErr(err)
@@ -201,7 +206,7 @@ func resourceCloudCredentialCreate(ctx context.Context, d *schema.ResourceData, 
 				d.Get("clientsecret").(string),
 				d.Get("subscriptionid").(string),
 				d.Get("tenantid").(string),
-				3)
+				credType)
 			if err != nil {
 				log.Printf("Error while create azure credentials  %s", err.Error())
 				return diag.FromErr(err)
@@ -209,6 +214,7 @@ func resourceCloudCredentialCreate(ctx context.Context, d *schema.ResourceData, 
 			d.SetId(s.ID)
 		} else if d.Get("providertype").(string) == "MINIO" {
 			if d.Get("awscredtype").(string) == "rolearn" {
+				// Discuss this path with @thirumal-rafay
 				if d.Get("rolearn").(string) == "" {
 					return diag.FromErr(fmt.Errorf("RoleARN cannot be empty"))
 				}

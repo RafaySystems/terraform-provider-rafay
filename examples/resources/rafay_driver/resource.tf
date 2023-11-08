20cdf0414d6923f1b36055b9bbaf61c7e2cae928
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2023-11-07 17:54:13 -0800

    Added EM docs and build fix for resource_aks_cluster_v3.go

---
+++ examples/resources/rafay_driver/resource.tf
@@ -0,0 +1,33 @@
+resource "rafay_driver" "driver" {
+  metadata {
+    name    = var.name
+    project = var.project
+  }
+  spec {
+    config {
+      type            = var.type
+      timeout_seconds = 100
+      max_retry_count = 3
+      container {
+        image     = var.image
+        arguments = ["--log-level=3"]
+        commands  = ["run main.go"]
+        image_pull_credentials {
+          password = "gibberesh"
+          registry = "hub.docker.io"
+          username = "randomuser"
+        }
+        kube_config_options {
+          kube_config    = "path/to/kubeconfig.json"
+          out_of_cluster = true
+        }
+        kube_options {
+          labels = {
+            "name" : "terraform"
+          }
+          resources = ["pods", "deployments"]
+        }
+      }
+    }
+  }
+}

b6b4df52c63cf466145b48afb848de06715c61d9
stephan benny <59401113+stephan-rafay@users.noreply.github.com>
2024-03-10 20:11:53 -0700

    fix error in example

--- examples/resources/rafay_workload_cd_operator/resource.tf
+++ examples/resources/rafay_workload_cd_operator/resource.tf
@@ -30,7 +30,7 @@ resource "rafay_workload_cd_operator" "operatordemo" {
     workload {
       name = "hello"
       chart_git_repo_path = "/hello-common/hello-0.1.3.tgz"
-      chart_helm_repo_name = "hello-repo"
+      chart_git_repo_name = "hello-repo"
       helm_chart_version = "0.1.3"
       helm_chart_name = "hello"
       path_match_pattern = "/:project/:workload/:namespace"
@@ -65,21 +65,6 @@ resource "rafay_workload_cd_operator" "operatordemo" {
       delete_action = "delete"
       placement_labels = {
         "httpecho-eu" = "enabled"
-      }
-    }
-
-    workload {
-      name = "hello"
-      chart_git_repo_path = "/hello-common/hello-0.1.3.tgz"
-      chart_helm_repo_name = "hello-repo"
-      helm_chart_version = "0.1.3"
-      helm_chart_name = "hello"
-      path_match_pattern = "/:project/:workload/:namespace"
-      base_path = "hello-common"
-      include_base_value = true
-      delete_action = "delete"
-      placement_labels = {
-        "hello" = "enabled"
       }
     }
    

ccfb54de5100e472ef16993f9282455aa9a241b7
Akshay Gaikwad <akshay.gaikwad@rafay.co>
2024-07-15 16:46:50 +0530

    Update user-agent for GKE and AKS v3 resources
    Signed-off-by: Akshay Gaikwad <akshay.gaikwad@rafay.co>

--- rafay/resource_gke_cluster.go
+++ rafay/resource_gke_cluster.go
@@ -17,7 +17,6 @@ import (
 	"github.com/RafaySystems/rafay-common/proto/types/hub/commonpb"
 	"github.com/RafaySystems/rafay-common/proto/types/hub/infrapb"
 	"github.com/RafaySystems/rctl/pkg/config"
-	"github.com/RafaySystems/rctl/pkg/versioninfo"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	schema "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -100,7 +99,7 @@ func resourceGKEClusterV3Upsert(ctx context.Context, d *schema.ResourceData, m i
 	log.Println(">>>>>> CLUSTER: ", cluster)
 
 	auth := config.GetConfig().GetAppAuthProfile()
-	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, TF_USER_AGENT)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -188,7 +187,7 @@ func resourceGKEClusterV3Read(ctx context.Context, d *schema.ResourceData, m int
 	}
 
 	auth := config.GetConfig().GetAppAuthProfile()
-	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent())
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, TF_USER_AGENT)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -233,7 +232,7 @@ func resourceGKEClusterV3Delete(ctx context.Context, d *schema.ResourceData, m i
 	}
 
 	auth := config.GetConfig().GetAppAuthProfile()
-	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, versioninfo.GetUserAgent(), options.WithInsecureSkipVerify(auth.SkipServerCertValid))
+	client, err := typed.NewClientWithUserAgent(auth.URL, auth.Key, TF_USER_AGENT, options.WithInsecureSkipVerify(auth.SkipServerCertValid))
 	if err != nil {
 		return diag.FromErr(err)
 	}

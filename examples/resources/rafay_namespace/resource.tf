bf6c19c96977c8c2bee24953897685d3088fcb1c
RKD <rkd@rafay.co>
2024-10-15 09:59:44 -0700

    RC-37900 - Moved data example to a different file

--- examples/resources/rafay_namespace/resource.tf
+++ examples/resources/rafay_namespace/resource.tf
@@ -125,28 +125,3 @@ resource "rafay_namespace" "tfdemonamespacewithmesh" {
   }
 }
 
-#Example of data namespace 
-data "rafay_namespace" "tfdemonamespace1" {
-  metadata {
-    name = "tfdemonamespace1"
-    project = "terraform"
-  }
-}
-
-output "tfdemonamespace_out" {
-  description = "spec"
-  value       = data.rafay_namespace.tfdemonamespace1.spec
-}
-
-#Example of data namespaces 
-data "rafay_namespaces" "all" {
-  metadata {
-    project = "terraform"
-  }
-}
-
-output "allnamespaces" {
-  description = "spec"
-  value       = data.rafay_namespaces.all
-}
-

ad8bace8ea499a49002409371e41ed8ed4a31837
ankit <ankit@rafay.co>
2024-11-06 13:56:08 +0530

    - change expiry type to float64 - include timezone field
    (cherry picked from commit 2ed6538fe63fc16c5ec9ddc1eebd1a7d31b046ce)

--- examples/resources/rafay_break_glass_access/resource.tf
+++ examples/resources/rafay_break_glass_access/resource.tf
@@ -6,14 +6,21 @@ resource "rafay_breakglassaccess" "test_user" {
     groups {
       group_expiry {
         expiry     = 7
-        name      = "grp3"
+        name      = "grp-2"
+        timezone = "America/Los_Angeles"
       }
       group_expiry {
         expiry     = 8
-        name      = "grp1"
+        name      = "grp-1"
         start_time = "2024-09-20T08:00:00Z"
       }
       user_type = "local"
     }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      spec[0].groups[0].group_expiry[0].start_time
+    ]
   }
 }

f3335e77fbcdf36da92659a1a16745779acfaafd
bharath-rafay <bharath.reddy@rafay.co>
2024-12-27 11:58:23 +0530

    RC-38616 : disable ebs csi driver flag

--- examples/resources/rafay_eks_cluster/providers.tf
+++ examples/resources/rafay_eks_cluster/providers.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     rafay = {
       version = ">= 0.1"
-      source = "registry.terraform.io/RafaySystems/rafay"
+      source = "registry.terraform.io/rafay/rafay"
     }
   }
 }
 
 variable "rafay_config_file" {
   description = "rafay provider config file for authentication"
-  default     = "<rafay-config-json-file>"
+  default     = "/home/ujwal/.rafay/cli/config.json"
   sensitive   = true
 }
 
-provider "rafay" {
-  provider_config_file = var.rafay_config_file
-}
+# provider "rafay" {
+#   provider_config_file = var.rafay_config_file
+# }

12d6967d7767632d1b16cc4c602421f1fff3bd2b
bharath-rafay <102665488+bharath-rafay@users.noreply.github.com>
2025-01-17 17:37:21 +0530

    Merge pull request #784 from RafaySystems/RC-39754
    RC-39754: FactSet: Implement a New Resource for Cluster Sharing with a single Project

---
+++ examples/resources/rafay_cluster_sharing_single/resource.tf
@@ -0,0 +1,8 @@
+#cluster sharing example, share with one project
+# resource "rafay_cluster_sharing_single" "demo-terraform-one" {
+#   clustername = "rauto-cm-452356829"
+#   project = "defaultproject"
+#   sharing {
+#       projectname = "rautocdktf-project-755252"
+#   }
+# }

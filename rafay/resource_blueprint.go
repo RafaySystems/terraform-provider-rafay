ed80bf16459a7bb069896b7ab714600189fa34f7
ankit <ankit@rafay.co>
2025-03-10 11:08:30 +0530

    fix blueprint diff in terraform plan

--- rafay/resource_blueprint.go
+++ rafay/resource_blueprint.go
@@ -202,6 +202,10 @@ func resourceBluePrintRead(ctx context.Context, d *schema.ResourceData, m interf
 	if tfBlueprintState.Spec != nil && tfBlueprintState.Spec.Sharing != nil && !tfBlueprintState.Spec.Sharing.Enabled && bp.Spec.Sharing == nil {
 		bp.Spec.Sharing = &commonpb.SharingSpec{}
 		bp.Spec.Sharing.Enabled = false
+		bp.Spec.Sharing.Projects = tfBlueprintState.Spec.Sharing.Projects
+	}
+	if tfBlueprintState.Spec != nil && tfBlueprintState.Spec.NamespaceConfig != nil && bp.Spec != nil && bp.Spec.NamespaceConfig != nil {
+		bp.Spec.NamespaceConfig.SyncType = tfBlueprintState.Spec.NamespaceConfig.SyncType
 	}
 
 	// XXX Debug

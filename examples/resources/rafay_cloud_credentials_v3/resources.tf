44eddadcc25e8a1c850473de31c7c37595ce9d8f
vishalv-rafay <vishal.shetty@rafay.co>
2025-04-01 22:41:04 -0700

    RC-34905. Added sample example for aks credentials

--- examples/resources/rafay_cloud_credentials_v3/resources.tf
+++ examples/resources/rafay_cloud_credentials_v3/resources.tf
@@ -18,3 +18,27 @@ resource "rafay_cloud_credentials_v3" "tftestcredentials" {
     }
   }
 }
+
+
+resource "rafay_cloud_credentials_v3" "tftestazurecredentials2" {
+   metadata {
+       name    = "sample-aws"
+       project = "sample-project"
+   }
+  spec {
+    type     = "ClusterProvisioning"
+    provider = "azure"
+    credentials {
+      tenant_id       = "tenant_id"
+      subscription_id = "subscription_id"
+      client_id       = "client_id"
+      client_secret   = "client_secret"
+    }
+    sharing {
+      enabled = false
+      projects {
+        name = "*"
+      }
+    }
+  }
+}

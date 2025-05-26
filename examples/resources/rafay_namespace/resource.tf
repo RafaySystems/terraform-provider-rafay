be5fea1e0a7f40ccb71784ede775918d08dc0eb0
rafay-vivek <vivek@rafay.co>
2025-05-26 17:02:08 +0530

    RC-39950: Added support for ephemeral_storage_limits and ephemeral_storage_requests.

--- examples/resources/rafay_namespace/resource.tf
+++ examples/resources/rafay_namespace/resource.tf
@@ -80,21 +80,23 @@ resource "rafay_namespace" "namespace" {
       }
     }
     resource_quotas {
-      config_maps              = "10"
-      cpu_limits               = "8000m"
-      memory_limits            = "16384Mi"
-      cpu_requests             = "4000m"
-      memory_requests          = "8192Mi"
-      gpu_requests             = "10"
-      gpu_limits               = "10"
-      persistent_volume_claims = "2"
-      pods                     = "30"
-      replication_controllers  = "5"
-      secrets                  = "10"
-      services                 = "10"
-      services_load_balancers  = "4"
-      services_node_ports      = "4"
-      storage_requests         = "10Gi"
+      config_maps                = "10"
+      cpu_limits                 = "8000m"
+      memory_limits              = "16384Mi"
+      cpu_requests               = "4000m"
+      memory_requests            = "8192Mi"
+      gpu_requests               = "10"
+      gpu_limits                 = "10"
+      persistent_volume_claims   = "2"
+      pods                       = "30"
+      replication_controllers    = "5"
+      secrets                    = "10"
+      services                   = "10"
+      services_load_balancers    = "4"
+      services_node_ports        = "4"
+      storage_requests           = "10Gi"
+      ephemeral_storage_limits   = "15Mi"
+      ephemeral_storage_requests = "5Mi"
     }
     network_policy_params {
       network_policy_enabled = true

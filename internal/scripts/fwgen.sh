3011e1cc784d3aeda197906cedcd09a36f32cdfd
Akshay Gaikwad <akshay.gaikwad@rafay.co>
2025-08-23 20:52:23 +0530

    Added more EKS fields

--- internal/scripts/fwgen.sh
+++ internal/scripts/fwgen.sh
@@ -15,3 +15,4 @@ sed -i 's/"metadata2"/"metadata"/g' internal/resource_eks_cluster/eks_cluster_re
 sed -i 's/"iam2"/"iam"/g' internal/resource_eks_cluster/eks_cluster_resource_gen.go
 sed -i 's/"iam_node_group_with_addon_policies2"/"iam_node_group_with_addon_policies"/g' internal/resource_eks_cluster/eks_cluster_resource_gen.go
 sed -i 's/"tolerations2"/"tolerations"/g' internal/resource_eks_cluster/eks_cluster_resource_gen.go
+sed -i 's/"iam3"/"iam"/g' internal/resource_eks_cluster/eks_cluster_resource_gen.go

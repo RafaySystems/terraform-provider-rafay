dc71aa7740ae9c673a12fd08ccabbf4e3987ccbf
krishna-rafay <shreekrishna@rafay.co>
2026-02-10 14:26:01 +0530

    fix for negative case

--- tests/integration/negative/resource_eks_cluster_empty_null_negative_test.go
+++ tests/integration/negative/resource_eks_cluster_empty_null_negative_test.go
@@ -93,7 +93,7 @@ func TestAccNegEKSCluster_NullClusterName_RequiredError(t *testing.T) {
 					  }
 					}
 				`,
-				ExpectError: regexp.MustCompile(`(?s)Missing Configuration for Required Attribute.*"cluster\.0\.metadata\.0\.name"`),
+				ExpectError: regexp.MustCompile(`(?s)Required Attribute.*metadata.*name`),
 			},
 		},
 	})
@@ -220,7 +220,7 @@ func TestAccNegEKSCluster_NullCloudProvider_RequiredError(t *testing.T) {
 					  }
 					}
 				`,
-				ExpectError: regexp.MustCompile(`(?s)Missing Configuration for Required Attribute.*"cluster\.0\.spec\.0\.cloud_provider"`),
+				ExpectError: regexp.MustCompile(`(?s)Required Attribute.*spec.*cloud_provider`),
 			},
 		},
 	})
@@ -304,7 +304,7 @@ func TestAccNegEKSCluster_NullRegion_RequiredError(t *testing.T) {
 					  }
 					}
 				`,
-				ExpectError: regexp.MustCompile(`(?s)Missing Configuration for Required Attribute.*"cluster_config\.0\.metadata\.0\.region"`),
+				ExpectError: regexp.MustCompile(`(?s)Required Attribute.*metadata.*region`),
 			},
 		},
 	})
